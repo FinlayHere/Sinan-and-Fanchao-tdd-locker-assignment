6fe828f65dcceef75a3ed7b03e5d1eceb516ff5e
fanchaokong <finlaykong@126.com>
2020-03-25 21:56:27 +0800

    [fanchao]: Make Test successful

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -1,8 +1,22 @@
 package cn.xpbootcamp.gilded_rose;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Locker {
+    public Locker() {
+    }
+
+    private Map<Ticket,Bag> container = new HashMap<Ticket,Bag>();
+
 
     public Ticket receive(Bag bag) {
-        return null;
+        Ticket ticket = new Ticket();
+        this.container.put(ticket,bag);
+        return ticket;
+    }
+
+    public boolean include(Bag bag) {
+        return this.container.containsValue(bag);
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -4,13 +4,25 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LockerTest {
     @Test
-    public void should_return_ticket_when_store_bag_given_bag(){
+    public void should_return_a_new_ticket_when_store_bag_given_bag(){
         Locker locker = new Locker();
         Bag bag = new Bag();
-        assertThat(locker.receive(bag)).isInstanceOf(Ticket.class);
+        Ticket ticket = locker.receive(bag);
+        assertThat(ticket).isInstanceOf(Ticket.class);
     }
+
+    @Test
+    public void should_bag_in_locker_when_after_store_bag_given_bag(){
+        Locker locker = new Locker();
+        Bag bag = new Bag();
+        locker.receive(bag);
+        assertTrue(locker.include(bag));
+    }
+
+
 
 }
