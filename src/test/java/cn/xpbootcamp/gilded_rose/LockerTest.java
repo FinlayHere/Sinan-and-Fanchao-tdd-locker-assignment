51355cbe6d7be7458de0d8dbbaf2eaa5bed64e1c
fanchaokong <finlaykong@126.com>
2020-03-25 21:04:50 +0800

    [fanchao]:Create Entity Bag and Ticket

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -2,11 +2,14 @@ package cn.xpbootcamp.gilded_rose;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LockerTest {
     @Test
     public void should_return_ticket_when_store_bag_given_bag(){
+        Locker locker = new Locker();
+        Bag bag = new Bag();
         assertThat(locker.receive(bag)).isInstanceOf(Ticket.class);
     }
 
