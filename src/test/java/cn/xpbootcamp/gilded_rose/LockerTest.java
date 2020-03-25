ef42f2611be39534a63a034f01409dbe8ce45a65
fanchaokong <finlaykong@126.com>
2020-03-25 20:59:04 +0800

    [fanchao]:assert give bag return ticket

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -2,8 +2,12 @@ package cn.xpbootcamp.gilded_rose;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LockerTest {
     @Test
-    public void should_return_ticket_when_store_bag_given_bag
+    public void should_return_ticket_when_store_bag_given_bag(){
+        assertThat(locker.receive(bag)).isInstanceOf(Ticket.class);
+    }
 
 }
