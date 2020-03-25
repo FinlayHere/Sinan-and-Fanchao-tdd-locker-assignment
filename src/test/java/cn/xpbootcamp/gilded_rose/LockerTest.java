8bcaf2af386b23f63ad9641404c5f9d3629bfe90
fanchaokong <finlaykong@126.com>
2020-03-25 22:03:43 +0800

    [fanchao]: when locker receive bag every time the ticket should be different

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -3,13 +3,16 @@ package cn.xpbootcamp.gilded_rose;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LockerTest {
+    Locker locker = new Locker();
     @Test
     public void should_return_a_new_ticket_when_store_bag_given_bag(){
-        Locker locker = new Locker();
         Bag bag = new Bag();
         Ticket ticket = locker.receive(bag);
         assertThat(ticket).isInstanceOf(Ticket.class);
@@ -17,10 +20,19 @@ public class LockerTest {
 
     @Test
     public void should_bag_in_locker_when_after_store_bag_given_bag(){
-        Locker locker = new Locker();
         Bag bag = new Bag();
         locker.receive(bag);
         assertTrue(locker.include(bag));
+    }
+
+    @Test void should_tickets_be_different_when_every_time_store_bag_given_more_than_two_ticket(){
+        Set<Ticket> tickets = new HashSet<Ticket>();
+        for (int i =0;i < 100;i++){
+            Bag bag = new Bag();
+            Ticket ticket = locker.receive(bag);
+            tickets.add(ticket);
+        }
+        assertThat(tickets.size()).isEqualTo(100);
     }
 
 
