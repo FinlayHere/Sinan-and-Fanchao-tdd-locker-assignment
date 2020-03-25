d9c122e7db2456cfca99557fc734e40af162bbb5
fanchaokong <finlaykong@126.com>
2020-03-25 22:30:56 +0800

    [fanchao] feat: consider capacity here

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -4,16 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Locker {
+    private int capacity;
     public Locker() {
+        this.capacity = Integer.MAX_VALUE;
+    }
+
+    public Locker(int capacity) {
+        this.capacity = capacity;
     }
 
     private Map<Ticket,Bag> container = new HashMap<Ticket,Bag>();
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Map<Ticket, Bag> getContainer() {
+        return container;
+    }
+
+    public void setContainer(Map<Ticket, Bag> container) {
+        this.container = container;
+    }
 
     public Ticket receive(Bag bag) {
-        Ticket ticket = new Ticket();
-        this.container.put(ticket,bag);
-        return ticket;
+        if (this.container.size() < capacity) {
+            Ticket ticket = new Ticket();
+            this.container.put(ticket,bag);
+            return ticket;
+        }
+        return null;
     }
 
     public boolean include(Bag bag) {
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -19,6 +19,16 @@ public class LockerTest {
     }
 
     @Test
+    public void should_not_store_bag_when_store_bag_and_no_more_capacity_given_capacity_and_bag(){
+        int capacity = 5;
+        Locker locker = new Locker(capacity);
+        for (int i = 0;i < 10;i++){
+            locker.receive(new Bag());
+        }
+        assertThat(locker.getContainer().size()).isEqualTo(5);
+    }
+
+    @Test
     public void should_bag_in_locker_when_after_store_bag_given_bag(){
         Locker locker = new Locker();
         Bag bag = new Bag();
@@ -38,6 +48,7 @@ public class LockerTest {
         assertThat(tickets.size()).isEqualTo(100);
     }
 
+
     @Test
     public void should_locker_no_longer_contain_bag_when_user_take_their_bag_away_given_ticket(){
         Locker locker = new Locker();
@@ -47,6 +58,9 @@ public class LockerTest {
         assertThat(locker.include(bag)).isEqualTo(false);
 
     }
+
+//    @Test
+//    public void should_locker_
 
 
 
