22cb3e4af274c593f01b671240f58ac16cdfc6a0
fanchaokong <finlaykong@126.com>
2020-03-25 22:10:55 +0800

    [fanchao] test: when user take their bag away then bag should no longer in locker

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -10,9 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LockerTest {
-    Locker locker = new Locker();
     @Test
     public void should_return_a_new_ticket_when_store_bag_given_bag(){
+        Locker locker = new Locker();
         Bag bag = new Bag();
         Ticket ticket = locker.receive(bag);
         assertThat(ticket).isInstanceOf(Ticket.class);
@@ -20,12 +20,15 @@ public class LockerTest {
 
     @Test
     public void should_bag_in_locker_when_after_store_bag_given_bag(){
+        Locker locker = new Locker();
         Bag bag = new Bag();
         locker.receive(bag);
         assertTrue(locker.include(bag));
     }
 
-    @Test void should_tickets_be_different_when_every_time_store_bag_given_more_than_two_ticket(){
+    @Test
+    public void should_tickets_be_different_when_every_time_store_bag_given_more_than_two_ticket(){
+        Locker locker = new Locker();
         Set<Ticket> tickets = new HashSet<Ticket>();
         for (int i =0;i < 100;i++){
             Bag bag = new Bag();
@@ -33,6 +36,16 @@ public class LockerTest {
             tickets.add(ticket);
         }
         assertThat(tickets.size()).isEqualTo(100);
+    }
+
+    @Test
+    public void should_locker_no_longer_contain_bag_when_user_take_their_bag_away_given_ticket(){
+        Locker locker = new Locker();
+        Bag bag = new Bag();
+        Ticket ticket = locker.receive(bag);
+        locker.takeBag(ticket);
+        assertThat(locker.include(bag)).isEqualTo(false);
+
     }
 
 
