7af7a53b7f52ca78140550d35ab175dc543efb88
fanchaokong <finlaykong@126.com>
2020-03-26 17:49:45 +0800

    [fanchao]: rewrite description

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -13,13 +13,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class LockerTest {
     /*
     ### ----存包裹----
-    - Given locker 最大容量 20，已存包裹包裹0-19（0，19，10），when 存包裹，then 寄存成功 返回ticket
-    - Given locker 最大容量 20，已存包裹20，when 存包裹，then 寄存失败 返回异常locker已经满了
+    - Given locker 最大容量 20，已经使用的容量为 0，when 存包裹，then 寄存成功 返回ticket
+    - Given locker 最大容量 20，已经使用的容量为 10，when 存包裹，then 寄存成功 返回ticket
+    - Given locker 最大容量 20，已经使用的容量为 19，when 存包裹，then 寄存成功 返回ticket
+    - Given locker 最大容量 20，已经使用的容量为 20，when 存包裹，then 寄存失败 返回异常locker已经满了
 
     ### ----取包裹----
-    - Given ticket 有效，when 取包裹，then 取包成功 ticket失效
-    - Given ticket 无效，when 取包裹，then 取包失败 返回异常 无效票据
+    - Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 得到同一个包裹
+    - Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 ticket失效
+    - Given 一个包裹 和它对应的 ticket，when 使用同一张 ticket 取两次包裹，then 取包失败 抛出异常 无效票据
      */
+
+    // Given locker 最大容量 20，已经使用的容量为 0，when 存包裹，then 寄存成功 返回ticket
     @Test
     public void should_return_ticket_when_deposit_parcel_given_locker_max_capacity_20_available_capacity_20() {
         // Given 容量是20的locker 当前可用容量是20 和一个待存入的包裹
@@ -30,6 +35,7 @@ public class LockerTest {
         assertThat(ticket).isInstanceOf(Ticket.class);
     }
 
+    // Given locker 最大容量 20，已经使用的容量为 10，when 存包裹，then 寄存成功 返回ticket
     @Test
     public void should_return_ticket_when_deposit_parcel_given_locker_max_capacity_20_available_capacity_10() {
         // Given 容量是20的locker 当前可用容量是10 和一个待存入的包裹
@@ -41,6 +47,7 @@ public class LockerTest {
         assertThat(ticket).isInstanceOf(Ticket.class);
     }
 
+    // Given locker 最大容量 20，已经使用的容量为 19，when 存包裹，then 寄存成功 返回ticket
     @Test
     public void should_return_ticket_when_deposit_parcel_given_locker_max_capacity_20_available_capacity_1() {
         // Given 容量是20的locker 当前可用容量是1 和一个待存入的包裹
@@ -52,6 +59,7 @@ public class LockerTest {
         assertThat(ticket).isInstanceOf(Ticket.class);
     }
 
+    // Given locker 最大容量 20，已经使用的容量为 20，when 存包裹，then 寄存失败 返回异常locker已经满了
     @Test
     public void should_throw_Error_when_deposit_parcel_given_locker_max_capacity_20_available_0() {
         // Given 容量是20的locker 当前可用容量为0
@@ -64,6 +72,7 @@ public class LockerTest {
                 "Locker full cannot save parcel anymore");
     }
 
+    // Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 得到同一个包裹
     @Test
     public void should_return_parcel_which_map_by_ticket_when_use_ticket_take_parcel_when_effective_ticket_and_certain_parcel() {
         // Given certain parcel and effective ticket
@@ -76,6 +85,7 @@ public class LockerTest {
         assertThat(parcel.getId()).isEqualTo(certainParcel.getId());
     }
 
+    // Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 ticket失效
     @Test
     public void should_invalidate_ticket_when_use_ticket_take_parcel_when_effective_ticket() {
         // Given effective ticket
@@ -87,6 +97,7 @@ public class LockerTest {
         assertThat(locker.getContainer().containsKey(ticket.getId())).isFalse();
     }
 
+    // Given 一个包裹 和它对应的 ticket，when 使用同一张 ticket 取两次包裹，then 取包失败 抛出异常 无效票据
     @Test
     public void should_throw_InvalidatedTicketException_when_take_parcel_given_invalidated_ticket() {
         // Given invalidated ticket
--- task/locker-tasking.md
+++ task/locker-tasking.md
@@ -1,8 +1,13 @@
+## Tasking
 ### ----存包裹----
-- Given locker 最大容量 20，已存包裹包裹0-19（0，19，10），when 存包裹，then 寄存成功 返回ticket
-- Given locker 最大容量 20，已存包裹20，when 存包裹，then 寄存失败 返回异常locker已经满了
+- Given locker 最大容量 20，已经使用的容量为 0，when 存包裹，then 寄存成功 返回ticket
+- Given locker 最大容量 20，已经使用的容量为 10，when 存包裹，then 寄存成功 返回ticket
+- Given locker 最大容量 20，已经使用的容量为 19，when 存包裹，then 寄存成功 返回ticket
+- Given locker 最大容量 20，已经使用的容量为 20，when 存包裹，then 寄存失败 返回异常locker已经满了
 
 ### ----取包裹----
-- Given ticket 有效，when 取包裹，then 取包成功 ticket失效 
-- Given ticket 无效，when 取包裹，then 取包失败 返回异常 无效票据
+- Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 得到同一个包裹 
+- Given 一个包裹 和它对应的 ticket，when 使用ticket 取包裹，then 取包裹成功 ticket失效
+- Given 一个包裹 和它对应的 ticket，when 使用同一张 ticket 取两次包裹，then 取包失败 抛出异常 无效票据
+
 
