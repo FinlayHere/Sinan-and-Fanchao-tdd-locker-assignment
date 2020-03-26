90afaf80cadf5191b49dda7c823d441025236aa4
fanchaokong <finlaykong@126.com>
2020-03-26 17:30:45 +0800

    [fanchao]: change store function the key should be the id of ticket and clean code

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class Locker {
     private int capacity;
+    private Map<String,Parcel> container = new HashMap<>();
+
     public Locker() {
         this.capacity = Integer.MAX_VALUE;
     }
@@ -13,42 +15,20 @@ public class Locker {
         this.capacity = capacity;
     }
 
-    private Map<Ticket,Parcel> container = new HashMap<Ticket,Parcel>();
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public Map<Ticket, Parcel> getContainer() {
-        return container;
-    }
-
-    public void setContainer(Map<Ticket, Parcel> container) {
-        this.container = container;
-    }
-
     public Ticket receive(Parcel parcel) {
         if (this.container.size() < capacity) {
             Ticket ticket = new Ticket();
-            this.container.put(ticket,parcel);
+            this.container.put(ticket.getId(),parcel);
             return ticket;
         }
         throw new LockerFullException("Locker full cannot save parcel anymore");
     }
 
-    public boolean include(Parcel parcel) {
-        return this.container.containsValue(parcel);
-    }
 
     public Parcel takeParcel(Ticket ticket) {
-        if (ticket.getId() != null){
-            Parcel parcel = this.container.get(ticket);
-            this.container.remove(ticket);
-            ticket.setId(null);
+        if (container.containsKey(ticket.getId())){
+            Parcel parcel = this.container.get(ticket.getId());
+            this.container.remove(ticket.getId());
             return parcel;
         }
         throw new InvalidatedTicketException("Invalidated ticket");
