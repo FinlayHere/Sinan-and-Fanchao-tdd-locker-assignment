db2a49e8d97ca90e77e926339bf966ec4999874e
fanchaokong <finlaykong@126.com>
2020-04-02 19:11:04 +0800

    [fanchao]: run 4th test successfully

--- src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/RobotTest.java
@@ -68,20 +68,22 @@ public class RobotTest {
 
     /**
      * 取且成功
-     * Given ticket(valid), two lockers and robot, when take parcel, then return parcel
+     * Given ticket(valid), locker and robot, when take parcel, then return parcel
      */
     @Test
     public void should_return_parcel_when_take_parcel_given_valid_ticket() {
         // Given
         ArrayList<Locker> managedLockers = new ArrayList<>();
         managedLockers.add(new Locker(1));
+        managedLockers.add(new Locker(1));
         LockerManageRobot lockerManageRobot = new LockerManageRobot(managedLockers);
-        Parcel parcel = new Parcel();
-        Ticket ticket = lockerManageRobot.recieve(parcel);
-        // When
-        assertThat(lockerManageRobot.receive(ticket)).isEqualTo(parcel);
+        Parcel firstParcel = new Parcel();
+        Parcel secondParcel = new Parcel();
 
+        Ticket firstTicket = lockerManageRobot.recieve(firstParcel);
+        Ticket secondTicket = lockerManageRobot.recieve(secondParcel);
+        // When and Then
+        assertThat(lockerManageRobot.receive(firstTicket)).isEqualTo(firstParcel);
+        assertThat(lockerManageRobot.receive(secondTicket)).isEqualTo(secondParcel);
     }
-
-
 }
